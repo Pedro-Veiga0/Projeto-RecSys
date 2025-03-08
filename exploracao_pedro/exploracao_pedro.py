867d7e6608795698d9edbed01fe8d3754e37e3d9
Pedro <pedroaveiga@proton.me>
2025-03-08 19:01:25 +0000

    + plots

--- exploracao_pedro/exploracao_pedro.py
+++ exploracao_pedro/exploracao_pedro.py
@@ -4,12 +4,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt 
 
 # CASA
-# movies = pd.read_csv(r'C:\Users\veigu\OneDrive\Ambiente de Trabalho\Projeto RecSys\movies_utf8.csv', delimiter=';')
-# ratings = pd.read_csv(r'C:\Users\veigu\OneDrive\Ambiente de Trabalho\Projeto RecSys\ratings.csv', delimiter=';')
+movies = pd.read_csv(r'C:\Users\veigu\OneDrive\Ambiente de Trabalho\Projeto RecSys\exploracao_pedro\movies_utf8.csv', delimiter=';')
+ratings = pd.read_csv(r'C:\Users\veigu\OneDrive\Ambiente de Trabalho\Projeto RecSys\exploracao_pedro\ratings.csv', delimiter=';')
 
 # portatil 
-movies = pd.read_csv(r'C:\Users\Pedro Afonso\Desktop\Trabalhos UM\1º Ano\2º Semestre\PIM\Projeto-RecSys\exploracao_pedro\movies_utf8.csv', delimiter=';')
-ratings = pd.read_csv(r'C:\Users\Pedro Afonso\Desktop\Trabalhos UM\1º Ano\2º Semestre\PIM\Projeto-RecSys\exploracao_pedro\ratings.csv', delimiter=';')
+# movies = pd.read_csv(r'C:\Users\Pedro Afonso\Desktop\Trabalhos UM\1º Ano\2º Semestre\PIM\Projeto-RecSys\exploracao_pedro\movies_utf8.csv', delimiter=';')
+# ratings = pd.read_csv(r'C:\Users\Pedro Afonso\Desktop\Trabalhos UM\1º Ano\2º Semestre\PIM\Projeto-RecSys\exploracao_pedro\ratings.csv', delimiter=';')
   
 # print(movies.head())
 # print(ratings.head())
@@ -18,13 +18,16 @@ junta = movies.merge(ratings, on='imdb_title_id')
 
 # print(junta.head())
 
-# missing_values = junta.isnull().sum()
+missing_values = junta.isnull().sum()
 # missing_values_m = movies.isnull().sum()
 # missing_values_r = ratings.isnull().sum()
 # print('MOVEIS', missing_values_m)
 # print('RATINGS',missing_values_r)
 
 # print(junta.info())
-
-sns.countplot(movies, x="date_published")
+# genero = movies['genre'].str.split(',').str[0].str.strip()
+# sns.countplot(movies, x=genero, stat="percent", palette="husl")
+intervalos = pd.cut(movies['duration'], bins=[0, 30, 60, 90, 120, float('inf')], labels=['0-30', '31-60', '61-90', '91-120', '120+'])
+sns.kdeplot(movies, x='avg_vote', hue=intervalos, fill=True, palette='husl') 
+plt.tight_layout()
 plt.show()
